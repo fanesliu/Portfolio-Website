a80a0f2c2119ea2b830f779a93c6d6269d86542e
Fanes Liu <152252867+fanesliu@users.noreply.github.com>
2024-06-01 23:00:23 +0700

    Update HeroSection.jsx

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -9,7 +9,7 @@ export const HeroSection = () => {
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to via-cyan-200 bg-pink-600">
-              Hello, I'm{" "}
+              Hello, I&rsquo;m{" "}
             </span>
             <br></br>
             <TypeAnimation
